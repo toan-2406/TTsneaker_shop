10b2ff22ef2287ef5b1b7567606525abc1baa66e
toan-2406 <57821796+toan-2406@users.noreply.github.com>
2021-08-06 17:54:18 +0700

    first-commit
    first-commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Helmet from '../components/Helmet'
+const Home = () => {
+    return (
+        <Helmet title="Trang chủ">
+            Home
+        </Helmet>
+    )
+}
+
+export default Home
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Route,Switch } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Cart from '../pages/Cart'
+import Product from '../pages/Product'
+import Catalog from '../pages/Catalog'
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/catalog/:slug'  component={Product}/>
+            <Route path='/catalog'  component={Catalog}/>
+            <Route path='/cart'  component={Cart}/>
+
+        </Switch>
+    )
+}
+
+export default Routes
