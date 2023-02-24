dd7e49bb9595e689e3479cffe896374078e39940
toan-2406 <ntoan2884@gmail.com>
2023-02-24 15:26:59 +0700

    update

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,41 +1,11 @@
-import React, { useState, useEffect, useCallback } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import { Link } from "react-router-dom";
 import banner from '../assets/images/banner.jpg'
-const HeroSlider = (props) => {
-  const data = props.data;
-
-  const setTimeOut = props.timeout ? props.timeout : 3000;
-
-  const [activeSlider, setActiveSlider] = useState(0);
-
-  // const NextActiveSlider = () => {
-  //     const index = activeSlider + 1 === data.length ? 0 : activeSlider + 1
-  //     setActiveSlider(index)
-  // }
-  const NextActiveSlider = useCallback(() => {
-    const index = activeSlider + 1 === data.length ? 0 : activeSlider + 1;
-    setActiveSlider(index);
-  }, [activeSlider, data]);
-  const PrevActiveSlider = () => {
-    const index = activeSlider - 1 < 0 ? data.length - 1 : activeSlider - 1;
-    setActiveSlider(index);
-  };
-
-  useEffect(() => {
-    if (props.auto) {
-      const slideAuto = setInterval(() => {
-        NextActiveSlider();
-      }, setTimeOut);
-      return () => {
-        clearInterval(slideAuto);
-      };
-    }
-  }, [NextActiveSlider, setTimeOut, props]);
+const HeroSlider = () => {
 
   return (
     <div className="hero-slider ">
-      <img src={banner}/>
+      <img src={banner} alt="banner"/>
       <div className="hero-slider__item__btn">
         <Link to="/catalog">
           <span>Shop now</span> <i className="bx bxs-cart-alt"></i>
@@ -44,46 +14,4 @@ const HeroSlider = (props) => {
     </div>
   );
 };
-
-HeroSlider.propTypes = {
-  data: PropTypes.array.isRequired,
-  control: PropTypes.bool,
-  auto: PropTypes.bool,
-  timeout: PropTypes.number,
-};
-
-const HeroSliderColors = (props) => (
-  <div
-    className={`hero-slider__control__btn__color__selection bg-${props.item.color}`}
-  ></div>
-);
-
-const HeroSliderItem = (props) => (
-  <div className={`hero-slider__item ${props.active ? "active" : ""}`}>
-    <div className="hero-slider__item__info">
-      <div className="hero-slider__item__info__brand">
-        <span>{props.item.brand}</span>
-      </div>
-      <div
-        className={`hero-slider__item__info__shoes color-${props.item.color}`}
-      >
-        <span>{props.item.shoes} </span>
-      </div>
-      <div className="hero-slider__item__info__description">
-        <span>{props.item.description}</span>
-      </div>
-      <div className="hero-slider__item__info__rating">
-        <i className="bx bxs-star"></i>
-        <i className="bx bxs-star"></i>
-        <i className="bx bxs-star"></i>
-        <i className="bx bxs-star"></i>
-        <i className="bx bxs-star"></i>
-      </div>
-    </div>
-    <div className="hero-slider__item__image image">
-      <img src={props.item.image} alt={props.item.brand + props.item.shoes} />
-    </div>
-  </div>
-);
-
 export default HeroSlider;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,14 +1,9 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 
 import Helmet from "../components/Helmet";
-import Grid from "../components/Grid";
-import ProductCard from "../components/ProductCard";
 import productData from "../assets/fake-data/products";
-import category from "../assets/fake-data/category";
-import CheckBox from "../components/CheckBox";
-import colors from "../assets/fake-data/product-colors";
-import Size from "../assets/fake-data/product-size";
-import Button from "../components/Button";
+
+
 import InfinityList from "../components/InfinityList";
 
 const Catalog = () => {
@@ -28,17 +23,17 @@ const Catalog = () => {
     let temp = productList;
 
     if (filter.category.length > 0) {
-      temp = temp.filter((e) => filter.category.includes(e.categorySlug));
+      temp = temp.filter((e) => filter.category?.includes(e.categorySlug));
     }
     if (filter.color.length > 0) {
       temp = temp.filter((e) => {
-        const check = e.colors.find((color) => filter.color.includes(color));
+        const check = e.colors.find((color) => filter.color?.includes(color));
         return check !== undefined;
       });
     }
     if (filter.size.length > 0) {
       temp = temp.filter((e) => {
-        const check = e.size.find((size) => filter.size.includes(size));
+        const check = e.size.find((size) => filter.size?.includes(size));
         return check !== undefined;
       });
     }
@@ -48,55 +43,6 @@ const Catalog = () => {
   useEffect(() => {
     updateProduct();
   }, [updateProduct]);
-  //Lọc sản phẩm
-  const filterSelect = (type, checked, item) => {
-    if (checked) {
-      switch (type) {
-        case "CATEGORY":
-          setFilter({
-            ...filter,
-            category: [...filter.category, item.categorySlug],
-          });
-          break;
-        case "COLOR":
-          setFilter({ ...filter, color: [...filter.color, item.color] });
-          break;
-        case "SIZE":
-          setFilter({ ...filter, size: [...filter.size, item.size] });
-          break;
-        default:
-      }
-    } else {
-      switch (type) {
-        case "CATEGORY":
-          const newCategory = filter.category.filter(
-            (e) => e !== item.categorySlug
-          );
-          setFilter({ ...filter, category: newCategory });
-          break;
-        case "COLOR":
-          const newColor = filter.color.filter((e) => e !== item.color);
-          setFilter({ ...filter, color: newColor });
-          break;
-        case "SIZE":
-          const newSize = filter.size.filter((e) => e !== item.size);
-          setFilter({ ...filter, size: newSize });
-          break;
-        default:
-      }
-    }
-  };
-  //xóa bộ lọc
-  const clearFilter = () => setFilter(initFilter);
-
-  const filterRef = useRef(null);
-
-  //show/hide button
-  const showHide = () => {
-    filterRef.current.classList.add("active");
-  };
-
-  const removeFilter = () => filterRef.current.classList.remove("active");
 
   return (
     <Helmet title="Sản phẩm">
